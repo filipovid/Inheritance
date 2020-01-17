e0108f34c821761fb0b7f1fad97b85b94322faf4
Filip <lord.vidojevic@gmail.com>
2020-01-17 03:00:57 +0100

    Initial Commit

---
+++ Person/Person/Person.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+        private string _name;
+        private int _age;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public virtual string Name
+        {
+            get { return _name; }
+
+            protected set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Name's length should be not less than 3 symbols!");
+                }
+
+                _name = value;
+            }
+        }
+
+        public virtual int Age
+        {
+            get { return _age; }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Age must be positive!");
+                }
+
+                _age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {_name}, Age: {_age}";
+        }
+    }
+}
---
+++ Person/Person/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Child child = new Child(name, age);
+                Console.WriteLine(child);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
